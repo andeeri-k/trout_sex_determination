5129c977d097f1d2ea64e323eb463762dc0b8b2a
@ndre! (AK) <andeeri@protonmail.com>
2024-04-29 17:45:17 +0200

    Update data_checker.py

--- scripts/data_checker.py
+++ scripts/data_checker.py
@@ -1,4 +1,5 @@
 # Data checker for model_xgboost.py
+# There is no error handling, code is tailored for use in xgboost program.
 # Coded by @ndre! RATM!
 # Should you have any questions: andeeri@protonmail.com / https://github.com/andeeri-k
 
@@ -7,16 +8,44 @@ import pandas as pd
 from tabulate import tabulate
 ################################
 
+
 def data_checker(data: pd.DataFrame):
-    """Function to print statistics of the data"""
+    """
+    Print statistics of the data.
+
+    Args:
+        data (pd.DataFrame): Input DataFrame containing marker data.
+
+    Returns:
+        int: Always returns None.
+    """
+    # Define a function to calculate mean absolute error
     def mean_absolute_error(marker_list: list, sex_list: list):
-        return sum(abs(el1 - (el2+1)) for el1, el2 in zip(marker_list, sex_list) if el1 != 5) / len([i for i in marker_list if i != 5])
+        """
+        Calculate the mean absolute error.
+
+        Args:
+            marker_list (list): List of marker values.
+            sex_list (list): List of sex values.
+
+        Returns:
+            float: Mean absolute error.
+        """
+        return sum(abs(marker - (sex + 1)) for marker, sex in zip(marker_list, sex_list) if marker != 5) / len([i for i in marker_list if i != 5])
+
+    # Print basic statistics
     print(f'Data checker:')
-    print(f'Number of fish: {data[0].count()}')
+    print(f'Number of fish: {data.shape[0]}')
     print(f'Ratio of males/females: {data[0].value_counts()[0]}/{data[0].value_counts()[1]}')
-    tab_dic = {'col1': list(range(1, 16)), 'col2': [], 'col3': []}
-    for i in range(1,16):
-        tab_dic['col2'].append(round(mean_absolute_error(data[i], data[0]), 3))
-        tab_dic['col3'].append(round(len(data[data[i]==5]) / len(data[i]), 3))
-    print(tabulate(tab_dic, headers = ['Marker', 'Error rate', 'Missing rate'], tablefmt='pretty'))
+
+    # Calculate error and missing rates
+    tab_data = {'Marker': list(range(1, 16)), 'Error rate': [], 'Missing rate': []}
+    for i in range(1, 16):
+        error_rate = round(mean_absolute_error(data[i], data[0]), 3)
+        missing_rate = round(len(data[data[i] == 5]) / len(data[i]), 3)
+        tab_data['Error rate'].append(error_rate)
+        tab_data['Missing rate'].append(missing_rate)
+
+    # Print tabulated data
+    print(tabulate(tab_data, headers='keys', tablefmt='pretty'))
     return None
