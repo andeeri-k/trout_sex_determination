1195189277a33e60a7d710a09adc87ca89e3864f
@ndre! (AK) <andeeri@protonmail.com>
2024-04-29 19:22:27 +0200

    Add files via upload

---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+python3 simulate_data.py
+python3 model_xgboost.py sim_5.dat sim_5.bin > sim_5.log
+python3 model_xgboost.py sim_50.dat sim_50_model.bin > sim_50.log
+python3 model_xgboost.py sim_real.dat sim_real.bin > sim_real.log
+python3 model_xgboost.py sim_random.dat sim_random.bin > sim_random.log
