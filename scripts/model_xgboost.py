d191301e97e21149344d931d7476ea8f7cb689b5
@ndre! (AK) <andeeri@protonmail.com>
2025-01-02 14:14:24 +0100

    Update model_xgboost.py
    some small fine-tuning (comments, imports and so on)

--- scripts/model_xgboost.py
+++ scripts/model_xgboost.py
@@ -9,23 +9,23 @@ import numpy as np
 import pandas as pd
 import sys
 import xgboost as xgb
-from sklearn import metrics
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import KFold
-from sklearn.model_selection import train_test_split
 from statistics import mean
+
 # Custom module
 try:
     from data_checker import data_checker
+
     checker = True
 except ImportError:
     checker = False
     pass
 #####################################################
 
-data_file = sys.argv[1] # your data
-out_model_filename = sys.argv[2] # file name for the trained model
+data_file = sys.argv[1]  # your data
+out_model_filename = sys.argv[2]  # file name for the trained model
 # data_file = 'sim_random.dat'
 # out_model_filename = 'sim_random_xgboost_model.bin'
 
@@ -37,10 +37,8 @@ y = dat[0].copy(deep=True)  # target vector
 X = dat[list(range(1, 16))].copy(deep=True)  # feature matrix
 X.replace(5, np.nan, inplace=True)  # set missing SNPs (5) to 'not a number'
 
-
 # Initiate Extreme Gradient Boost Binary Logistics Classifier with default parameters
 clf = xgb.XGBClassifier(objective='binary:logistic', min_child_weight=0)
-
 
 # Run GridSearchCV to find the best suitable parameters for the model
 param_grid = {
@@ -50,43 +48,44 @@ param_grid = {
     'colsample_bytree': [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 }
 
-gscv = GridSearchCV(estimator=clf, param_grid=param_grid, scoring='accuracy', cv=5, verbose=1) # initiate GridSerchCV
-gscv.fit(X, y) # run GridSerchCV
-best_params = gscv.best_params_ # store the best params
-print("Best Parameters:", best_params) # print the best params
-best_model = gscv.best_estimator_ # store the best model
-best_model.save_model(out_model_filename) # save trained model
+gscv = GridSearchCV(estimator=clf, param_grid=param_grid, scoring='accuracy', cv=5, verbose=1)  # initiate GridSerchCV
+gscv.fit(X, y)  # run GridSerchCV
+best_params = gscv.best_params_  # store the best params
+print("Best Parameters:", best_params)  # print the best params
+best_model = gscv.best_estimator_  # store the best model
+best_model.save_model(out_model_filename)  # save trained model
 
 
 # Function to perform n-fold cross validation
 def n_fold_validation(X: pd.DataFrame, y: pd.Series, model_params: dict, n_folds: int, iterations: int) -> None:
-    """"Function to perform n-fold validation (n_folds) n times (iterations)
+    """Function to perform n-fold validation (n_folds) n times (iterations)
     Args:
         X (pd.DataFrame): Input DataFrame containing marker data
         y (pd.DataFrame): Input DataFrame containing sex data
         model_params (dict): Input dictionary with model parameters
         n_folds (int): number of data splits
-        interations (int): number of repeated rounds
-    
+        iterations (int): number of repeated rounds
+
     Returns:
         Always returns None
-    
+
     """
     print(f'\nPerforming {n_folds}-f cross validation')
-    aggregator_of_means = [] # list of accuracy means
-    clf_cv = xgb.XGBClassifier(objective='binary:logistic', min_child_weight=0, **model_params) # initiate model
+    aggregator_of_means = []  # list of accuracy means
+    clf_cv = xgb.XGBClassifier(objective='binary:logistic', min_child_weight=0, **model_params)  # initiate model
     for i in range(iterations):
-      folds = KFold(n_splits=n_folds, shuffle=True, random_state=802)
-      cv_accuracy = cross_val_score(clf_cv, X, y, cv=folds, scoring='accuracy')
-      aggregator_of_means.append(cv_accuracy.mean())
-    mean_ag = mean(aggregator_of_means) # mean accuracy in n-folds
-    test_group = len(y)//n_folds # size of the test group
-    miscls = int(test_group * (1 - mean_ag)) # number of misclassified samples
-    perc_miscls = int(miscls / test_group * 100) # percent of misclassified samples
+        folds = KFold(n_splits=n_folds, shuffle=True, random_state=802)
+        cv_accuracy = cross_val_score(clf_cv, X, y, cv=folds, scoring='accuracy')
+        aggregator_of_means.append(cv_accuracy.mean())
+    mean_ag = mean(aggregator_of_means)  # mean accuracy in n-folds
+    test_group = len(y) // n_folds  # size of the test group
+    miscls = int(test_group * (1 - mean_ag))  # number of misclassified samples
+    perc_miscls = int(miscls / test_group * 100)  # percent of misclassified samples
     print(f'Mean {n_folds}-f cross validation replicated {iterations} times was {mean(aggregator_of_means):.5f}')
     print(f'Average number misclassified samples: {miscls}')
     print(f'Average percentage misclassified samples: {perc_miscls}%')
     return None
+
 
 # Perform n-fold cross validation
 # 5-fold CV replicated 20 times (100 rounds in total)
